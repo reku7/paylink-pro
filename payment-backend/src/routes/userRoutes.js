140168ad7ffc066b199b2e1f1d8eea9a9e3eb7cb
reku7 <rekiklegese@gmail.com>
2026-01-27 20:41:55 +0300

    define updateMe

--- payment-backend/src/routes/userRoutes.js
+++ payment-backend/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import { getMe } from "../controllers/userController.js";
+import { getMe, updateMe } from "../controllers/userController.js";
 
 const router = express.Router();
 
